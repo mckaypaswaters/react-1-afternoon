b2937e75d893db80e287042ffda7344ae84e75ab
McKay Waters <mckaypaswaters@gmail.com>
2019-09-01 14:36:52 -0600

    finished FilterObject

--- src/components/Topics/FilterObject.js
+++ src/components/Topics/FilterObject.js
@@ -27,18 +27,33 @@ class FilterObject extends Component {
             filteredArray: []
         }
     }
+
+    handleChange(val) {
+        this.setState({ userInput: val });
+      }
     
+      filterFriends(prop) {
+        let unfilteredArray = this.state.unfilteredArray;
+        let filteredArray = [];
     
+        for ( let i = 0; i < unfilteredArray.length; i++ ) {
+          if ( unfilteredArray[i].hasOwnProperty(prop) ) {
+            filteredArray.push(unfilteredArray[i]);
+          }
+        }
+    
+        this.setState({ filteredArray: filteredArray });
+      }
     
     
     render(){
         return (
             <div className='puzzleBox filterObjectPB'> 
             <h4>Filter Object</h4>
-            <span className='puzzleText'></span>
+            <span className='puzzleText'> Original: { JSON.stringify(this.state.unfilteredArray, null, 10) }</span>
             <input className='inputLine' onChange={ (e) => this.handleChange(e.target.value) }></input>
-            <button className='confirmationButton'></button>
-            <span className='resultsBox filterObjectRB'></span>
+            <button className='confirmationButton' onClick= { () => this.filterFriends(this.state.userInput) }>Filter</button>
+            <span className='resultsBox filterObjectRB'> Filtered: { JSON.stringify(this.state.filteredArray, null, 10) }</span>
         </div>  
         )
     }
