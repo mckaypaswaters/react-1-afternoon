01358d651069baf60b4a8e45eb8d9b814e6f4509
McKay Waters <mckaypaswaters@gmail.com>
2019-09-01 16:31:07 -0600

    finished palindrome

--- src/components/Topics/Palindrome.js
+++ src/components/Topics/Palindrome.js
@@ -1,9 +1,39 @@
 import React, { Component } from 'react';
 
 class Palindrome extends Component {
+    constructor() {
+        super()
+        this.state = {
+            userInput: '',
+            palindrome: ''
+        }
+    }
+
+    handleChange(val){
+        this.setState({userInput: val})
+    }
+
+    isPalindrome() {
+        let emptyArray = []
+
+        emptyArray = this.state.userInput.split('')
+        emptyArray.reverse()
+        let checkPalin = emptyArray.join('')
+        console.log(checkPalin)
+        if (this.state.userInput === checkPalin){
+            this.setState({palindrome: 'true'})
+        } else this.setState({palindrome: 'false'})
+
+    }
+
     render(){
         return (
-            <p>Palindrome Component</p>
+            <div className='puzzleBox filterStringPB'>
+                <h4>Palindrome</h4>
+                <input className='inputLine' onChange={ e => this.handleChange(e.target.value)}></input>
+                <button className="confirmationButton" onClick={() => this.isPalindrome(this.state.userInput)}>Check</button>
+                <span className="resultsBox"> Palindrome: {this.state.palindrome}</span>
+            </div>
         )
     }
 }
